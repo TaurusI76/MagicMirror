99cf068f955a7be49a9d1a3c4d850077aec50958
TaurusI76 <philipp@cogslab.com>
2023-12-21 15:50:22 +0100

    Python syntax is stupid

--- source/SnapPrintSend.py
+++ source/SnapPrintSend.py
@@ -139,10 +139,10 @@ def OnInitContinue():
     updateResult = subprocess.Popen("./update.sh")
     updateReturnCode = updateResult.returncode
 
-    if updateReturnCode == 1
+    if updateReturnCode == 1:
         rebootAfterShutdown = True
         Shutdown();
-    else
+    else:
         print("Initialization continues...")
         global button
         button.when_pressed = TakePicture
