6e09bd2910760cf4c702743a1873d04632f032a6
Philipp Kuhn <philipp@cogslab.com>
2023-12-22 13:25:39 +0100

    Changed path to Version.txt in GetCurrentVersion() check.

--- source/VersionControl.py
+++ source/VersionControl.py
@@ -1,6 +1,7 @@
 
 def GetCurrentVersion():
-    file = open("../Version.txt", "r")
+    # Check the Version.txt file that lives inside the git-managed folder
+    file = open("../MagicMirror/Version.txt", "r")
     version = int(file.readline())
     file.close()
     return version
